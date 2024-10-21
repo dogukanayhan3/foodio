a30f6200f4403166f831fd6a46b3b12f6f9d4b39
Doğukan Ayhan <113291789+dogukanayhan3@users.noreply.github.com>
2024-10-22 02:26:14 +0300

    Update_baglanti.php

--- baglanti.php
+++ baglanti.php
@@ -1,9 +1,9 @@
 <?php
    
-   define('DB_USER', "dogukan");
-   define('DB_PASSWORD', "");
-   define('DB_DATABASE', "foodio");
-   define('DB_SERVER', "localhost");
+   define('DB_USER', "?");
+   define('DB_PASSWORD', "?");
+   define('DB_DATABASE', "?");
+   define('DB_SERVER', "?");
 
 $baglanti=mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
 mysqli_set_charset($baglanti,"UTF8");
